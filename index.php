b93ca98fbc09c79b68dae3dd17bd077450447984
ShadowFoxDelta <rexguerra45@hotmail.com>
2025-05-05 14:38:44 -0500

    mini actualización

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="CSS/CSS_L2P2/header.css">
 </head>
 <body>
-
+	<div><?php include("componentes/header.html")?></div><!--Incrustar header-->
 </body>
 </html>
