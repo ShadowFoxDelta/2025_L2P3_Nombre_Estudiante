9021cfc404d6e26b5a960023f42ed776f15e7fb9
ShadowFoxDelta <rexguerra45@hotmail.com>
2025-05-08 22:30:56 -0500

    Leve cambio

--- L2P2.php
+++ L2P2.php
@@ -4,9 +4,9 @@
 <head>
 	<title>Utiles Escolares</title>
 	<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="CSS/index/header.css">
-    <link rel="stylesheet" type="text/css" href="CSS/index/footer.css">
-	<link rel="stylesheet" type="text/css" href="CSS/index/index.css">
+	<link rel="stylesheet" type="text/css" href="CSS/L2P2/header.css">
+    <link rel="stylesheet" type="text/css" href="CSS/L2P2/footer.css">
+	<link rel="stylesheet" type="text/css" href="CSS/L2P2/L2P2.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
