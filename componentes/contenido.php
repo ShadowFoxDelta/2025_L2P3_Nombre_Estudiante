4ce9e2eed6d5d64ed27e508bfeba69137f3833dd
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2025-05-08 21:44:49 -0500

    documentacion

--- componentes/contenido.php
+++ componentes/contenido.php
@@ -26,12 +26,24 @@
 
 <body>
 
+    <!-- /**
+     * - El contenedor principal tiene la clase "contenido" y está estructurado utilizando el sistema de grillas de Bootstrap.
+
+     * - Dentro del formulario, hay dos secciones principales:
+     *   - La primera sección (de 8 columnas de ancho) incluye el contenido de "productos.php", 
+     *     que maneja un componente filas con los productos comprar.
+     *   - La segunda sección (de 4 columnas de ancho) incluye el contenido de "factura.php", 
+     *     que generara la factura resultante de todos los productos elegidos.
+
+     * - El formulario utiliza el método POST para el envío de datos, pero el atributo action está vacío, 
+     *   lo que significa que el formulario se enviará a la misma página.
+     */ -->
     <div class="container contenido">
 
         <form action="" method="post">
             <div class="row">
                 <div class="col-md-8">
-                    <?php include("./componentes/productos.php"); ?>
+                    <?php include("./componentes/productos.php"); ?> 
                 </div>
                 <div class="col-md-4">
                     <?php include("./componentes/factura.php"); ?>
