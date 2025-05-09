d45e99f8c7e0ded27b60cd0d9965f8e3f3331eb9
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2025-05-08 20:18:20 -0500

    arreglo de algo

--- index.php
+++ index.php
@@ -4,18 +4,20 @@
 <head>
 	<title>Utiles Escolares</title>
 	<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="CSS/CSS_L2P2/header.css">
-	<link rel="stylesheet" type="text/css" href="CSS/CSS_L2P2/index.css">
+	<link rel="stylesheet" type="text/css" href="CSS/index/header.css">
+    <link rel="stylesheet" type="text/css" href="CSS/index/footer.css">
+	<link rel="stylesheet" type="text/css" href="CSS/index/index.css">
 
 </head>
 
 <body>
-
 	<div><?php include("componentes/comunes/header.html") ?></div><!--Incrustar header-->
+
+	<div><?php include("componentes/comunes/jumbotron.html") ?></div><!--Incrustar header-->
 
 	<?php include("./componentes/contenido.php") ?>
 	
-	<div><?php include("componentes/comunes/footer.html") ?></div><!--Incrustar header-->
+	<?php include("componentes/comunes/footer.html") ?> <!--Incrustar foooter-->
 </body>
 
 </html>
