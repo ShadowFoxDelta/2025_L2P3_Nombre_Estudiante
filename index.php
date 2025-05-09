fe05f5e42d4e0b379a8a049228d0288c3e78bafe
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2025-05-08 21:15:02 -0500

    aregglando espaciado del footer

--- index.php
+++ index.php
@@ -4,15 +4,9 @@
 <head>
 	<title>Utiles Escolares</title>
 	<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
-<<<<<<< Updated upstream
 	<link rel="stylesheet" type="text/css" href="CSS/index/header.css">
     <link rel="stylesheet" type="text/css" href="CSS/index/footer.css">
 	<link rel="stylesheet" type="text/css" href="CSS/index/index.css">
-=======
-	<link rel="stylesheet" type="text/css" href="CSS/CSS_L2P2/header.css">
-	<link rel="stylesheet" type="text/css" href="CSS/CSS_L2P2/index.css">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
->>>>>>> Stashed changes
 
 </head>
 
@@ -21,7 +15,9 @@
 
 	<?php include("componentes/comunes/jumbotron.html") ?><!--Incrustar header-->
 
-	<?php include("./componentes/contenido.php") ?>
+<div class="contenido">
+<?php include("./componentes/contenido.php") ?> <!--Incrustar contenido-->
+</div>
 
 	<?php include("componentes/comunes/footer.html") ?> <!--Incrustar foooter-->
 </body>
